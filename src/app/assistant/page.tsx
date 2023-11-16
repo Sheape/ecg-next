8e3e56f903d52ecf35d0c8e92652959c29cc8599
Sheape <86521166+Sheape@users.noreply.github.com>
2023-11-17 06:06:43 +0800

    feat: add upload button

--- src/app/assistant/page.tsx
+++ src/app/assistant/page.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
 import Navbar from "../../components/Navbar";
 
-
+const CreateNew = () => {
+  return (
+    <>
+      <div className="flex p-10 mx-auto items-center max-w-5xl min-h-[80vh]">
+        <div className="flex flex-col min-w-[30rem] min-h-[20rem] rounded-lg border-dashed border-2 p-6 justify-center items-center text-center">
+          <button className="bg-hr-primary text-white px-10 py-3 rounded-lg font-semibold">
+            <span className="material-icons text-[3rem]">upload_file</span>
+          </button>
+          <p className="text-hr-text">Upload digital ECG Recording</p>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default function Assistant() {
   return (
     <>
       <Navbar />
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1 bg-hr-accent">
-          <h1>Nice</h1>
-        </div>
-        <div>
-          <h2>Col 2</h2>
-        </div>
-        <div>
-          <h2>Col 3</h2>
-        </div>
-      </div>
+      <CreateNew />
     </>
   );
 }
